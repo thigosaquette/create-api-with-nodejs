f99d0de2bb5a627d9941c46097563ecc7417dd85
thigosaquette <thigo.saquette@gmail.com>
2022-12-12 09:36:30 -0300

    tsyringe was applied to manage instances

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+/* import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
@@ -12,4 +12,4 @@ export default (): CreateCategoryController => {
   );
 
   return createCategoryController;
-};
+}; */
